b5a4f84b33f598ab8c2f6be3c3820ddb43d80c3b
Michael Chen <michael.ld.chen@gmail.com>
2020-02-24 12:03:54 -0500

    Update Listing model

--- backend/src/models/Listing.ts
+++ backend/src/models/Listing.ts
@@ -1,10 +1,32 @@
 import mongoose, { Schema } from "mongoose";
 
+/**
+ * Find the distance between two latitude–longitude points, using the Haversine formula.
+ * This does not account for 
+ * Derived from https://stackoverflow.com/a/21623206/ by Salvador Dali, licensed under CC BY-SA 4.0.
+ * 
+ * @param lat1 latitude of point 1
+ * @param lon1 longitude of point 1
+ * @param lat2 latitude of point 2
+ * @param lon2 longitude of point 2
+ * @param km   whether to output result in kilometers or miles
+ */
+function distance(lat1: number, lon1: number, lat2: number, lon2: number, km: boolean = false): number {
+    const p = 0.017453292519943295;    // Math.PI / 180
+    const c = Math.cos;
+    const a = 0.5 - c((lat2 - lat1) * p)/2 + 
+            c(lat1 * p) * c(lat2 * p) * 
+            (1 - c((lon2 - lon1) * p))/2;
+    const twoR = km ? 12742 : 7918; // 2 * mean radius of the Earth
+
+    return twoR * Math.asin(Math.sqrt(a));
+}
+
 export type ListingDocument = mongoose.Document & {
-    lat: number,
-    lon: number,
-    capacity: number,
-    price: number
+    lat: number;
+    lon: number;
+    capacity: number;
+    price: number;
 };
 
 const listingSchema = new mongoose.Schema({
@@ -24,10 +46,27 @@ const listingSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+    startDate: {
+        type: Date,
+        default: new Date()
+    },
+    endDate: {
+        type: Date,
+        default: new Date(2200, 1, 1)
+    },
     price: {
         type: Number,
         required: true
     }
 }, { timestamps: true });
+
+listingSchema.statics.findNearby = async function (lat?: number, lon?: number, minCapacity: number = 1, maxPrice: number = Infinity, startDate: Date = new Date(2300, 1, 1), endDate: Date = new Date()) {
+    return await this.find({ 
+        remCapacity: { $gte: minCapacity },
+        price: { $lte: maxPrice },
+        startDate: { $lte: startDate },
+        endDate: { $gte: endDate },
+    });
+};
 
 export const Listing = mongoose.model<ListingDocument>("Listing", listingSchema);
