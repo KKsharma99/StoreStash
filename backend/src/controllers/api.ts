87f8738d955d8aa81b58d8dc515b80b4e01c4770
Michael Chen <michael.ld.chen@gmail.com>
2020-04-10 18:11:30 -0400

    Write better API documentation

--- backend/src/controllers/api.ts
+++ backend/src/controllers/api.ts
@@ -1,36 +1,81 @@
 "use strict";
 
 import graph from "fbgraph";
-import crypto from "crypto";
 import mongoose from "mongoose";
 import { Response, Request, NextFunction } from "express";
 import { UserDocument, User } from "../models/User";
 import { Listing, ListingDocument } from "../models/Listing";
 import { Rental, RentalDocument } from "../models/Rental";
 
-/**
- * Returns a random string of characters 0-9, a-f.
- * 
- * @param bytes number of bytes used for the string; each byte is two hex digits
- */
-function randomString(bytes: number) {
-    return crypto.randomBytes(bytes).toString("hex");
+// Some types just as a reference to understand the expected response from the API
+type ListingJson = {
+    host: any; // host ID
+    lat: number;
+    lon: number;
+    capacity: number;
+    remSpace: number;
+    startDate: string;
+    endDate: string;
+    price: number;
 }
 
+type UserJson = {
+    email: string;
+    password: string;
+    passwordResetToken: string;
+    passwordResetExpires: string; // date
+
+    facebook: string;
+    tokens: {accessToken: string; kind: string;}[];
+
+    profile: {
+        name: string;
+        gender: string;
+        location: string;
+        website: string;
+        picture: string;
+    };
+}
+
+type RentalJson = {
+    host: string;
+    renter: string;
+    listing: string;
+    boxes: number;
+    dropoff: string; // dropoff date
+    pickup: string; // pickup date
+    price: number;
+}
+
+// Some fields that will likely be in the response
+type MongoJson = {
+    _id: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+}
+
+/**
+ * POST /api/users/new
+ * @param req.body.email
+ * @param req.body.password
+ * Response: the new User
+ */
 export const newUser = async (req: Request, res: Response) => {
     try {
         let user;
-        if (req.query.random == "true" || req.body.random == "true") {
-            user = await (User as unknown as UserDocument).construct(`${randomString(5)}@gatech.edu`, randomString(8));
-        } else {
-            user = await (User as unknown as UserDocument).construct(req.body.email, req.body.password);
-        }
+        user = await (User as unknown as UserDocument).construct(req.body.email, req.body.password);
         res.json(user.toObject());
     } catch (err) {
         res.status(400).send(err);
     }
 };
 
+/**
+ * GET /api/users/:id/rentals
+ * @param {string} req.params.id User ID
+ * Response: Rental[]
+ */
 export const getRentalHistory = async (req: Request, res: Response) => {
     try {
         const history = await (Rental as unknown as RentalDocument).listRenterHistory(req.params.id);
@@ -40,6 +85,17 @@ export const getRentalHistory = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ * POST /api/listings/new
+ * @param {string} req.body.hostId
+ * @param {number} req.body.lat
+ * @param {number} req.body.lon
+ * @param {number} req.body.capacity
+ * @param {string} req.body.startDate
+ * @param {string} req.body.endDate
+ * @param {number} req.body.price
+ * Response: the new Listing
+ */
 export const newListing = async (req: Request, res: Response) => {
     try {
         const listing = await (Listing as unknown as ListingDocument).construct(req.body.hostId, req.body.lat, req.body.lon, req.body.capacity, new Date(req.body.startDate), new Date(req.body.endDate), req.body.price);
@@ -50,6 +106,11 @@ export const newListing = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ * GET /api/listings/:id
+ * @param {string} req.params.id
+ * Response: Listing
+ */
 export const getListing = async (req: Request, res: Response) => {
     try {
         const listing = await (await Listing.findById(req.params.id)).execPopulate();
@@ -59,6 +120,15 @@ export const getListing = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ * POST /api/listings/:id/rent
+ * @param {string} req.params.id Listing ID
+ * @param {string} req.body.renter Renter ID
+ * @param {number} req.body.boxes Number of boxes to be rented
+ * @param {Date} req.body.dropoff Dropoff date
+ * @param {Date} req.body.pickup Pickup date
+ * Response: the new Rental
+ */
 export const rentListing = async (req: Request, res: Response) => {
     try {
         const rental = await (Rental as unknown as RentalDocument).construct(mongoose.Types.ObjectId(req.params.id), req.body.renter, req.body.boxes, req.body.dropoff, req.body.pickup);
@@ -68,6 +138,15 @@ export const rentListing = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ * GET /api/listings/nearby
+ * @param {number} req.query.lat Latitude of where the user is
+ * @param {number} req.query.lon Longitude of where the user is
+ * @param {number} req.query.minCapacity Minimum capacity of a listing to show up in the results
+ * @param {number} req.query.maxPrice
+ * @param {string} req.query.startDate
+ * @param {string} req.query.endDate
+ */
 export const getNearby = async (req: Request, res: Response) => {
     try {
         const listings = await (Listing as unknown as ListingDocument).getNearby(req.query.lat, req.query.lon, req.query.minCapacity, req.query.maxPrice, new Date(req.query.startDate), new Date(req.query.endDate));
