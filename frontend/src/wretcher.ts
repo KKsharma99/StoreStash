89cc360d65a7702a20e07d5aba42ca1183dadcf4
Michael Chen <michael.ld.chen@gmail.com>
2020-04-30 01:54:42 -0400

    Remove template middlewear; doesn't fix things

--- frontend/src/wretcher.ts
+++ frontend/src/wretcher.ts
@@ -3,6 +3,6 @@ import wretch from "wretch";
 const wretcher = wretch()
     .url('http://localhost:3001')
     // .url("https://storestash.herokuapp.com")
-    .options({ credentials: "include", mode: "cors" });
+    // .options({ credentials: "include", mode: "cors" });
 
 export default wretcher;
